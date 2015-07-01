0617e0a7d231d122dfe259a0018e12a2043687d7
Logan <Logan@Lenovo-PC>
2015-06-30 21:09:34 -0600

    Merge origin/master
    Conflicts:
    	src/cit260/lhcz/monopofast/view/StartView.java

---
+++ test/cit260/lhcz/monopofast/control/HighScoreTest.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package cit260.lhcz.monopofast.control;
+
+import org.junit.Test;
+import static org.junit.Assert.*;
+
+/**
+ *
+ * @author Logan
+ */
+public class HighScoreTest {
+    
+    public HighScoreTest() {
+    }
+
+    /**
+     * Test of highScoreArray method, of class HighScore.
+     */
+   
+}
